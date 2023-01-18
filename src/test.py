e5fb96442f9a8d8cd091cbc74b6a3d999e8a7fc1
jonathanplangger <jonathanplangger@cmail.carleton.ca>
2023-01-18 07:00:39 -0500

    Push current non-working version of train.py

--- src/test.py
+++ src/test.py
@@ -1,20 +1,3 @@
-import os 
-import cv2
-
-label_dir = '../../datasets/Rellis-3D/00000/pylon_camera_node_label_id/'
-fileName = 'frame000000-1581624652_750.png'
-
-filePath = label_dir + fileName
-
-
-assert os.path.exists(filePath)
-
-# open the label file for examination
-label = cv2.imread(filePath)
-
-
-
-print(label[1100,10])
-
-
-
+import torch
+foo = torch.tensor([1,2,3])
+foo = foo.to('cuda')
