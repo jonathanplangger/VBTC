42e339af8d84da8938a3e91b3c1272c453803f8f
jonathanplangger <jonathanplangger@cmail.carleton.ca>
2023-01-24 07:34:00 -0500

    Add batch/img loading features to dataloader

--- src/dataloader.py
+++ src/dataloader.py
@@ -3,6 +3,7 @@ import os, json, cv2
 import time
 import tensorflow as tf 
 import numpy as np
+import random
 
 # ------------- Dataloader for the new dataset ------------- #
 class DataLoader(object):
@@ -28,6 +29,9 @@ class DataLoader(object):
         # store the obtained metadata as a parameter
         self.metadata = data
         self.num_classes = 20 # TODO update to depend on the dataset
+        self.size = len(self.metadata) # n# of elements in the entire dataset
+        self.height = int(self.metadata[0]["height"])
+        self.width = int(self.metadata[0]["width"])
 
 
     # --------------------------- Database Registrations --------------------------------------#
@@ -63,16 +67,57 @@ class DataLoader(object):
 
         return meta_files
 
-    
+    def randomizeOrder(self):
+        """
+            Randomizes the order of the metadata object. This will shuffle the elements in the dict in a random order
+            \n This will update the object metadata parameter and is NON reversable. Only use for training.
+        """    
+        random.shuffle(self.metadata) # shuffle the current order of the list
+
+    def load_frame(self, img_path, mask_path): 
+        """
+            Loads the given image alongside its segmentation mask. \n
+            ---------------------------\n
+            parameters:\n
+            img_path(str) = Path to the image 
+            mask_path(str) = Path to the mask file 
+            transform = transformation function to be applied to the incoming data
+        """
+        img = cv2.imread(img_path)
+        print(img)
+        mask = cv2.imread(mask_path)
+        return img, mask
+
+
+    def load_batch(self, idx, batch_size:int):
+        """
+            Load a batch of size "batch_size". Returns the images, annotation maps, and the newly updated index value
+        """
+
+        images = np.empty((batch_size, self.height, self.width, 3))
+        annMap = np.empty((batch_size, self.height, self.width, 3))
+
+        # load image and mask files
+        for i in range(batch_size): 
+            images[i], annMap[i] = self.load_frame(self.metadata[i]["file_name"], self.metadata[i]["sem_seg_file_name"])
+
+        # update the index value 
+        idx += batch_size
+
+        return images, annMap, idx
+
+
+
+
+
+
+
 
 
 # # --------- Testing the class above, REMOVE later ------------ #
 # rellis_path = "../../datasets/Rellis-3D/" #path ot the dataset directory
 
 # loader = DataLoader(rellis_path)
-# # print(loader.metadata[0]["file_name"])
-
-
-
-
+# print(loader.metadata[0]["file_name"])
+# images, annMap, idx = loader.load_batch(0, 3)
 
--- src/train.py
+++ src/train.py
@@ -5,18 +5,25 @@ from IPython.display import clear_output
 import matplotlib.pyplot as plt
 from dataloader import DataLoader
 import cv2
+import numpy as np 
 
 print("---------------------------------------------------------------\n")
-dataset, info = tfds.load('oxford_iiit_pet:3.*.*', with_info=True)
-
 db = DataLoader("./../../datasets/Rellis-3D/")
 
+# Global configuration variables
+BATCH_SIZE = 3
+TRAIN_LENGTH = len(db.metadata)
+STEPS_PER_EPOCH = TRAIN_LENGTH//BATCH_SIZE # n# of steps within the specific epoch.
 
-# Normalzie the color values into the 0,1 range 
+# method only works if the images are a consistent size (like Rellis)
+height = db.metadata[0]["height"]
+width = db.metadata[0]["width"]
+
+# Normalize the color values inot the 0,1 range
 def normalize(input_image, input_mask):
   input_image = tf.cast(input_image, tf.float32) / 255.0
   input_mask -= 1
-  return input_image, input_mask
+  return input_image.numpy(), input_mask
 
 
 
