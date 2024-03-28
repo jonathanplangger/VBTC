80848f261eeb0c927803600ab7b18587cd09f75c
jonathanplangger <jonathanplangger@cmail.carleton.ca>
2024-03-27 21:32:16 -0400

    Update README documentation

--- README.md
+++ README.md
@@ -4,17 +4,16 @@
 This repository contains the code for the implementation of Vision-based Terrain Classification Research as part of the Embedded Multi-Sensor Resarch Laboratory at Carleton University (Ottawa, ON). As a research repository, the code relevant to several aspects are included within this repository.
 Installation instructions for required software solutions and enviroment information is provided in the following sections of the README.MD. We additionally include the implementation instructions for our FCIoU paper below which can be used to recreate the comparative study and experiments detailed in the research manuscript.  
 
-##Environment Setup & Installation
-**Python** ver. **3.8.15**, **Pytorch** ver. **1.13.1+cu117**, & **TorchVision** ver. **0.14.1+cu117** were employed during development. Other, more recent versions of these may operate correctly, however only the specified versions were tested. 
-
-Other libraries employed are defined within the *requirements.txt* file included in this repository. Project dependencies can be installed directly through:
-```
-pip install -r requirements.txt
-```
+#### Publications
+Plangger, Jonathan, Mohamed Atia, and Hicham Chaoui. “FCIoU: A Targeted Approach for Improving Minority Class Detection in Semantic Segmentation Systems.” Machine Learning and Knowledge Extraction 5, no. 4 (November 23, 2023): 1746–59. https://doi.org/10.3390/make5040085.
 
 
+### Repository Structure
+The structure of the repository code is represented as: 
 
-#FCIoU: A Focal Class-based Intersection over Union (IoU) Approach to Improving Minority Class Detection Performance for Off-road Segmentation Systems
+![Structural block diagram of the code contained in the repository](github/RepoStructure.png)
+
+### FCIoU: A Focal Class-based Intersection over Union (IoU) Approach to Improving Minority Class Detection Performance for Off-road Segmentation Systems
 ## Abstract
 In this paper, we present a comparative study of modern semantic segmentation loss functions and their resultant impact when applied with state-of-the-art off-road datasets. Class imbalance, inherent in these datasets, presents a significant challenge to off-road terrain semantic segmentation systems. With numerous environment classes being extremely sparse and underrepresented, model training becomes inefficient and struggles to comprehend the infrequent minority
 classes. As a solution to this problem, loss functions have been configured to take class imbalance
@@ -22,15 +21,21 @@ into account and counteract this issue. To this end, we present a novel loss fun
 optimization of class-based Intersection over Union (IoU). The new loss function results in a general
 increase in class-based performance when compared to state-of-the-art targeted loss functions. 
 
-![Image Comparing the segmentation ability for each loss function for a single image](./figures/QualitativeResults.png)
-*Figure: Qualitative Representation of Model Performance for Given Loss Function* 
+### Getting Started 
+#### Environment Setup & Installation
+**Python** ver. **3.8.15**, **Pytorch** ver. **1.13.1+cu117**, & **TorchVision** ver. **0.14.1+cu117** were employed during development. Other, more recent versions of these may operate correctly, however only the specified versions were tested. 
+
+Other libraries employed are defined within the *requirements.txt* file included in this repository. Project dependencies can be installed directly through:
+```
+pip install -r requirements.txt
+```
+
+##### Running the program
+Once all the dependencies are installed and resolved, the code can be started by directly running the eval.py and train.py code. The specific configurations by these programs can be configured using the config/config_comparative_study.yaml.
 
 
 
-
-
-
-##Loss Functions
+### Loss Functions
 Loss function implementations employed within the study are defined in *loss.py*. Each loss function were recreated within this study to suit the task of multi-class segmentation. Further information regarding the specific loss functions are provided within the paper manuscript. 
 
 Selection of loss function for training can be configured through the *CRITERION* field in the *config_comparative_study.yaml* file in the *configs* directory. The possible selections are as follows: 
@@ -48,10 +53,22 @@ Selection of loss function for training can be configured through the *CRITERION
 Selecting a specific loss function in the *CRITERION* field results in the use of the corresponding loss function class. 
 </div>
 
-#Model Comparative Study
-This repository contains the source code for the models employed wihtin the comparative study. The links for the repositories are as follows: 
+### Models for Comparative Study
+Contained within the models/ directory is a selection of state-of-the-art semantic segmentation modeles employed in the comparative study. The original implementations of the models were obtained from: 
 
 HRNetv2 + OCR: https://github.com/HRNet/HRNet-Semantic-Segmentation
+DeepLabv3+: https://github.com/VainF/DeepLabV3Plus-Pytorch
+U-Net: https://amaarora.github.io/posts/2020-09-13-unet.html
+
+
+#### Model Configuration
+The configuration for the model utilized in training or evaluation can be selected and modified through the *MODEL_NAME* field in the configuration file. 
+
+The strings to select each of the specified model are as follows: 
+- *fciouvX*, where *X* refers to the desired version (fciouv1,etc.)
+- *deeplabv3plus*,  Deeplabv3+
+- *hrnet_ocr*, HRNetv2 & OCR
+
 
 
 
--- newmodel.py
+++ newmodel.py
@@ -5,129 +5,41 @@ from torchinfo import summary
 import unet
 import torch.autograd.profiler as profiler 
 from lib.partialconv.models.partialconv2d import PartialConv2d # import the partial convolutions
+# Add the resnet to the available backbone to run the program
+sys.path.append("models/DeepLabV3Plus-Pytorch/network/backbone/")
+from resnet import resnet34
+
 
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
-class Encoder(nn.Module): 
-    def __init__(self):
-        super().__init__()
-        self.input_handling = nn.Conv2d(in_channels=3, out_channels=1, kernel_size=3, stride=1, padding=1)
-    
-    def forward(self,x): 
-        return self.input_handling(x) 
-
-class TestLayer(nn.Module): 
+class NewModel(nn.Module):
     def __init__(self): 
         super().__init__()
-        self.encoder = Encoder()
+        self.peri = PeripheralUnit()  # get the peripheral viewing unit
 
-    def forward(self, x):
-        return self.encoder(x)
-    
+    def forward(self, x):  
+        return self.peri(x) 
 
-class NewModel(nn.Module): 
-    def __init__(self, num_classes = 19): 
-        super().__init__()
-        self.peripheral = PeripheralUnit()
-        self.central = CentralUnit()
-        self.conv1 = nn.Conv2d(in_channels=2, out_channels=1, kernel_size=1)
 
-    def forward(self, x):
-        peripheral, mask = self.peripheral(x)
-        central = self.central(x, mask)
-        cat = torch.cat((peripheral, central), 0)
-        cat = torch.permute(cat, (1,0,2,3)) # shift around before completing convolution
-        conv1 = self.conv1(cat)
-        conv1 = torch.permute(conv1, (1,0,2,3)) # shift the positions back to original location. 
-        return conv1 # remove the extra dimension
-
-class Lense(nn.Module): 
-    def __init__(self, in_channels, out_channels, kernel_size, padding, dilation): 
-        super().__init__()
-        self.conv = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, padding = padding, dilation = dilation)
-
-    def forward(self, x): 
-        return self.conv(x) 
-
-class CentralBlock(nn.Module): 
-    def __init__(self, in_channels, out_channels): 
-        super().__init__()
-        self.conv1 = Lense(in_channels=in_channels, out_channels=out_channels, kernel_size=7, padding=3, dilation = 1)
-        self.conv2 = Lense(in_channels=in_channels, out_channels=out_channels, kernel_size=7, padding=6, dilation = 2)
-        self.conv3 = Lense(in_channels=in_channels, out_channels=out_channels, kernel_size=7, padding=9, dilation = 3)
-        self.conv4 = Lense(in_channels=in_channels, out_channels=out_channels, kernel_size=7, padding=12, dilation = 4)
-        self.conv5 = Lense(in_channels=in_channels, out_channels=out_channels, kernel_size=7, padding=15, dilation = 5)
-    
-    def forward(self, x): 
-        conv1 = self.conv1(x)
-        conv2 = self.conv2(x)
-        conv3 = self.conv3(x)
-        conv4 = self.conv4(x)
-        conv5 = self.conv5(x)
-        # Stack the resulting convolutions together into one stack layer
-        return torch.cat((conv1, conv2, conv3, conv4, conv5), 1)
 
 class CentralUnit(nn.Module):
     # TODO: Update the names of each layer to be more fitting a variable (set) amount of channels 
     def __init__(self, depth = 6, num_classes=19): 
         super().__init__()
-        self.num_classes = num_classes # set the n# of classes which will be featured on output
-        self.cb1 = CentralBlock(3, depth)
-        self.conv1d1 = nn.Conv2d(in_channels=depth*5, out_channels=20, kernel_size=1) #intermediary layer convolutions
-        self.cb2 = CentralBlock(20, depth)
-        self.conv1d2 = nn.Conv2d(in_channels=depth*5, out_channels=num_classes, kernel_size=1)
-
 
     def forward(self, x, mask=torch.zeros(19,1200,1920)): 
-        # expand the tensor to include mask for all classes. 
-        mask = mask.repeat(self.num_classes,1,1) 
-        
-        #First stack calculation
-        s1 = self.cb1(x)
-        # apply k=1 convolution to obtain a single resulting map
-        c1 = self.conv1d1(s1)
-        s2 = self.cb2(c1)
-        # c2 = self.conv1d2(s2, mask_in = mask)
-        c2 = self.conv1d2(s2)
-
-        # Apply the mask to the convolution and return output
-        return torch.mul(c2, mask)
-
-
+        pass
 
 class PeripheralUnit(nn.Module): 
     def __init__(self): 
         super().__init__()
-        base = 10
-        kernel_size = 5
-        num_class = 19
-        self.topk = 10
-
-        self.peripheral = unet.UNet(
-            enc_chs=(3,base, base*2, base*4, base*8, base*16),
-            dec_chs=(base*16, base*8, base*4, base*2, base), 
-            out_sz=(1200,1920), retain_dim=True, num_class=num_class, kernel_size=kernel_size
-        )
+        backbone = resnet34() # use the resnet 34 as the backbone for the network
+        # remove avg pooling layers on output (constraining it to 1000 classes output)
+        self.backbone = nn.Sequential(*list(backbone.children())[:-2]) 
 
     def forward(self,x):
-        
-        # obtain the segmentatoin results for the peripheral backbone network
-        pred = self.peripheral(x)
-        # Get the probabilistic prediction for each class. 
-        prob = torch.softmax(pred, dim=1)
-        
-        max_vals,_ = prob.max(dim=1) # get the max values for each pixels
-        max_vals = max_vals.flatten() # flatten the matrix to make the topk operation work
-        num_k = self.topk/100*max_vals.shape[0] # obtain the n# of values to be represented 
-        _, topk = torch.topk(max_vals, dim = 0, k = int(num_k), largest=False)
+        return self.backbone(x)
 
-        # Create the mask based on the topk indexes. 
-        mask = torch.zeros(max_vals.shape, device=device)
-        mask.scatter_(0, topk, 1.)
-        mask = torch.reshape(mask, (1,prob.shape[2],prob.shape[3])) # reshape into the original dimensions
-    
-        return pred, mask
-    
 
 
 
